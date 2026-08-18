a0414e6aec61de14b2a51648bc7e0004bea5d3df
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-08-18 15:47:19 +0500

    coding

--- src/components/newarrial/Newarrial.jsx
+++ src/components/newarrial/Newarrial.jsx
@@ -96,8 +96,7 @@ function StarRating({ rating }) {
             height="14"
             viewBox="0 0 24 24"
             className={`star-icon ${type}`}
-          >
-          
+          >          
             <path d="M12 2l2.9 6.6 7.1.6-5.4 4.7 1.6 7-6.2-3.7L5.8 21l1.6-7L2 9.2l7.1-.6z" />
           </svg>
         );
